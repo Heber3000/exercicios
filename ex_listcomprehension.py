f7d2176195330c7b69b0f9b5f700f10b0c5f2229
heber3000 <heberlevy@gmail.com>
2022-11-29 16:20:58 -0300

    list comprehension

---
+++ ex_listcomprehension.py
@@ -0,0 +1,15 @@
+ex1 = [str(i) for i in range(10)]
+print(ex1)
+
+ex2 = [n for n in range(10)]
+print(ex2)
+
+
+lista = [1,2,3,4]
+ex3 = [i * 3 for i in lista]
+print(ex3)
+
+
+lista1 = [3,3,3]
+ex4 = [i for i in lista]
+print(ex4)
